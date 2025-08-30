7325f8b4527ec7ffeef86056b90025b495135935
fellipessanha <fellipe900@gmail.com>
2025-08-30 17:35:36 -0300

    improve parsing routine

--- metaheuristics/Exercises/src/Parsing.jl
+++ metaheuristics/Exercises/src/Parsing.jl
@@ -5,10 +5,9 @@ function make_problem_context_from_file(contents::IOStream)
     @info "package_count: $(package_count), dependency_count: $(dependency_count),\
     relation_count: $(relation_count), storage_size: $(storage_size)"
 
-    package_scores = parse_vector_line(contents |> readline)
-    @assert length(package_scores) == package_count
+    package_scores = parse_vector_line(contents |> readline, package_count)
 
-    dependency_weights = parse_vector_line(contents |> readline)
+    dependency_weights = parse_vector_line(contents |> readline, dependency_count)
     @assert length(dependency_weights) == dependency_count
 
     dependencies = map(parse_vector_line, contents |> readlines)
@@ -39,4 +38,11 @@ end
 function parse_vector_line(line::AbstractString)
     string_vector = filter(entry -> !isempty(entry), line |> split)
     return map(entry -> parse(Int, entry), string_vector)
+end
+
+function parse_vector_line(line::AbstractString, correct_count::Integer)
+    parsed_line = parse_vector_line(line)
+    @assert length(parsed_line) == correct_count "Parsed line has incorrect number of elements.\
+        Should have $(correct_count), found $(length(parsed_line))"
+    return parsed_line
 end
--- metaheuristics/Exercises/test/runtests.jl
+++ metaheuristics/Exercises/test/runtests.jl
@@ -2,13 +2,15 @@ using MetaheuristicsExercises
 using Test
 
 test_instances = ["prob-software-85-100-812-12180.txt"]
-test_instance_filepaths = Iterators.map(instance -> joinpath("..", "instances", instance), test_instances)
+test_instance_filepaths = Iterators.map(instance -> joinpath("instances", instance), test_instances)
 
 for instance in test_instance_filepaths
     contents = open(instance)
     context = MetaheuristicsExercises.make_problem_context_from_file(contents)
 
-    summary = "$(context.package_count)-$(context.dependency_count)-$(context.relation_count)-$(context.storage_size)"
+
+    context_variables = [context.package_count, context.dependency_count, context.relation_count, context.storage_size]
+    summary = join(context_variables, '-')
 
     @test occursin(summary, instance)
     @test context.storage_size == 12180
