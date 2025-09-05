f292227982e50fcd4c583d6e8f80373c2f60c9b7
fellipessanha <fellipe900@gmail.com>
2025-09-05 15:22:19 -0300

    update longest palindromic substring

--- personal/leetcode/go/longest_palindromic_substring.go
+++ personal/leetcode/go/longest_palindromic_substring.go
@@ -16,15 +16,13 @@ func isPalindrome(s string) bool {
 	return true
 }
 
-func find(s string, c rune, l int = 0, r int)
-
 func longestPalindrome(s string) string {
 	var currentLongest string = string(s[0])
 
 	for i, char := range s {
-		firstSearch := s[i:]
-		for j := strings.IndexRune(firstSearch[i+1:], char); j != 1; j = j + strings.IndexRune(firstSearch[j+1:], char) {
-			substr := firstSearch[i:j]
+		search := s[i:]
+		for j := strings.LastIndexByte(search, byte(char)); j < len(search) && j != -1; j = strings.LastIndexByte(search[:j], byte(char)) {
+			substr := search[:j+1]
 			println(substr)
 			if len(substr) > len(currentLongest) && isPalindrome(substr) {
 				currentLongest = substr
@@ -35,5 +33,7 @@ func longestPalindrome(s string) string {
 }
 
 func main() {
-	longestPalindrome("oi eu sou o goku")
+	found := longestPalindrome("oi eu sou o abba goku")
+	println("found: ", found)
+
 }
