dac3ce33089ed1c09a90d29009886e31811bcdaf
fellipessanha <fellipe900@gmail.com>
2025-09-20 15:12:35 -0300

    add new move types

---
+++ Exercises/src/apply_moves.jl
@@ -0,0 +1,19 @@
+function apply(problem::ProblemContext, solution::Solution, move::RemovePackageMove)
+    if !(move.package in solution.used_packages)
+        return solution
+    end
+
+    removed_dependencies = get_removed_dependencies_by_package(solution, move.package)
+
+    for dependency in removed_dependencies
+        if solution.used_dependencies[dependency] |> length <= 1
+            delete!(solution.used_dependencies, dependency)
+        else
+            pop!(solution.used_dependencies[dependency], package)
+        end
+    end
+
+    pop!(solution.used_packages, move.package)
+
+    return solution
+end
