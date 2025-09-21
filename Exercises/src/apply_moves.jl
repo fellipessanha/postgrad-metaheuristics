da8a4143aede5b10acf33eacec758a4816ce5e5e
fellipessanha <fellipe900@gmail.com>
2025-09-21 00:00:52 -0300

    return new Solution pointer

--- Exercises/src/apply_moves.jl
+++ Exercises/src/apply_moves.jl
@@ -4,16 +4,19 @@ function apply(problem::ProblemContext, solution::Solution, move::RemovePackageM
     end
 
     removed_dependencies = get_removed_dependencies_by_package(solution, move.package)
+    removed_weight = [problem.dependency_weights[i] for i in removed_dependencies] |> sum
 
+    used_dependencies::AbstractDict{Integer,AbstractSet{Integer}} = copy(solution.used_dependencies)
     for dependency in removed_dependencies
         if solution.used_dependencies[dependency] |> length <= 1
-            delete!(solution.used_dependencies, dependency)
+            delete!(used_dependencies, dependency)
         else
-            pop!(solution.used_dependencies[dependency], package)
+            pop!(used_dependencies[dependency], package)
         end
     end
 
-    pop!(solution.used_packages, move.package)
+    used_packages = copy(solution.used_packages)
+    pop!(used_packages, move.package)
 
-    return solution
+    return Solution(used_packages, used_dependencies, solution.weight - removed_weight)
 end
