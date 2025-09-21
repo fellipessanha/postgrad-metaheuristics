85e8503167abb301dad171ed78296864a30b9d52
Fellipe Pessanha <69586375+fellipessanha@users.noreply.github.com>
2025-09-21 13:07:32 -0300

    add FlipPackageMove operations (#11)
    * add FlipPackageMove operations
    
    * update tests

--- Implementation/src/moves/apply.jl
+++ Implementation/src/moves/apply.jl
@@ -39,3 +39,10 @@ function apply(problem::ProblemContext, solution::Solution, move::AddPackageMove
 
     return solution
 end
+
+function apply(problem::ProblemContext, solution::Solution, move::FlipPackageMove)
+    if move.package in solution.used_packages
+        return apply(problem, solution, RemovePackageMove(move.package))
+    end
+    return apply(problem, solution, AddPackageMove(move.package))
+end
