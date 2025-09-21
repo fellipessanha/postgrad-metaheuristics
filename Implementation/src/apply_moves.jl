f2cfa46dcda149fd9d65aaed838c0303e76eba96
fellipessanha <fellipe900@gmail.com>
2025-09-21 12:31:53 -0300

    add apply AddPackageMove

--- Implementation/src/apply_moves.jl
+++ Implementation/src/apply_moves.jl
@@ -19,3 +19,23 @@ function apply(problem::ProblemContext, solution::Solution, move::RemovePackageM
 
     return solution
 end
+
+function apply(problem::ProblemContext, solution::Solution, move::AddPackageMove)
+    if move.package in solution.used_packages
+        return solution
+    end
+
+    new_dependencies =
+        setdiff(get_dependencies_used_by_package(problem, move.package), keys(solution.used_dependencies))
+    for dependency in new_dependencies
+        dependency_packages                    = get(solution.used_dependencies, dependency, Set{Integer}())
+        solution.used_dependencies[dependency] = union(dependency_packages, move.package)
+    end
+
+    additional_weight = [problem.dependency_weights[dependency] for dependency in new_dependencies] |> sum
+    solution.weight   += additional_weight
+
+    push!(solution.used_packages, move.package)
+
+    return solution
+end
