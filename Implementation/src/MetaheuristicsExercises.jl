7a965f9416fdfecc5ed157f516dd51b057dd81f7
fellipessanha <fellipe900@gmail.com>
2025-10-03 20:25:14 -0300

    add AddDependencyMove

--- Implementation/src/MetaheuristicsExercises.jl
+++ Implementation/src/MetaheuristicsExercises.jl
@@ -7,8 +7,14 @@ include("evaluation.jl")
 include("moves/moves.jl")
 include("constructive.jl")
 
-export ProblemContext, make_problem_context_from_file, get_dependencies_used_by_package
+export ProblemContext, Solution, Move
+export make_problem_context_from_file, get_dependencies_used_by_package
 export get_all_used_dependencies, evaluate, generate_random_greedy_initial_solution
 export generate_greedy_initial_solution, generate_random_initial_solution
+export get_removed_dependencies_by_package
+
+export apply!
+export AddPackageMove, RemovePackageMove, FlipPackageMove
+export AddDependencyMove, RemoveDependencyMove
 
 end
